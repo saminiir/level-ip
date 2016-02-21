1b0b7499deb48bd879faeff347a837ae9adf5222
Sami Niiranen <saminiir@gmail.com>
2016-02-21 19:59:21 +0200

    Open basic TUN device

---
+++ main.c
@@ -0,0 +1,93 @@
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define CMDBUFLEN 100
+
+int run_cmd(char *cmd, ...)
+{
+    va_list ap;
+    char buf[CMDBUFLEN];
+    va_start(ap, cmd);
+    vsnprintf(buf, CMDBUFLEN, cmd, ap);
+
+    va_end(ap);
+
+    printf("%s\n", buf);
+
+    return system(buf);
+}
+
+int set_route(char *dev, char *cidr)
+{
+    return run_cmd("ip route add dev %s %s", dev, cidr);
+}
+
+int set_if_up(char *dev)
+{
+    return run_cmd("ip link set dev %s up", dev);
+}
+
+/*
+ * Taken from Kernel Documentation/networking/tuntap.txt
+ */
+int tun_alloc(char *dev)
+{
+    struct ifreq ifr;
+    int fd, err;
+
+    if( (fd = open("/dev/net/tun", O_RDWR)) < 0 ) {
+        printf("Cannot open TUN/TAP dev");
+        exit(1);
+    }
+
+    memset(&ifr, 0, sizeof(ifr));
+
+    /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
+     *        IFF_TAP   - TAP device
+     *
+     *        IFF_NO_PI - Do not provide packet information
+     */
+    ifr.ifr_flags = IFF_TUN;
+    if( *dev ) {
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+
+    if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ){
+        printf("ERR: Could not ioctl tun: %s\n", strerror(errno));
+        close(fd);
+        return err;
+    }
+
+    strcpy(dev, ifr.ifr_name);
+    return fd;
+}
+
+int main(int argc, char** argv) {
+    int tun_fd;
+    char buf[100];
+
+    char *dev = calloc(10, 1);
+    tun_fd = tun_alloc(dev);
+
+    if (set_if_up(dev) != 0) {
+        printf("ERROR when setting up if\n");
+            
+    }
+
+    if (set_route(dev, "10.0.0.0/24") != 0) {
+        printf("ERROR when setting route for if\n");
+    }
+
+    read(tun_fd, buf, 100);
+
+    free(dev);
+}
