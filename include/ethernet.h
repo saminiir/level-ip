5f44348e2126e13d50cf4f9f3195ae4979ab63c9
Sami Niiranen <saminiir@gmail.com>
2016-03-03 19:18:12 +0200

    Make eth header fields unsigned

--- include/ethernet.h
+++ include/ethernet.h
@@ -1,10 +1,16 @@
+#ifndef ETHERNET_H_
+#define ETHERNET_H_
+#include <linux/if_ether.h>
+
 struct eth_hdr 
 {
     unsigned char dst_mac[6];
     unsigned char src_mac[6];
-    short ethertype;
-    char* payload;
+    unsigned short ethertype;
+    unsigned char* payload;
 };
 
 struct eth_hdr* init_eth_hdr(char* buf);
 void print_eth_hdr(struct eth_hdr *ehdr);
+
+#endif
