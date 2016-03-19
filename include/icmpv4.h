1c94258d391c00d1025fa8f951e1a8fef6d19ad9
Sami Niiranen <saminiir@gmail.com>
2016-03-19 21:04:15 +0200

    Add first, nonworking version of ICMPv4 request handling

--- include/icmpv4.h
+++ include/icmpv4.h
@@ -4,6 +4,30 @@
 #include "syshead.h"
 #include "netdev.h"
 
+#define ICMP_V4_REPLY           0x00
+#define ICMP_V4_DST_UNREACHABLE 0x03
+#define ICMP_V4_SRC_QUENCH      0x04
+#define ICMP_V4_REDIRECT        0x05
+#define ICMP_V4_ECHO            0x08
+#define ICMP_V4_ROUTER_ADV      0x09
+#define ICMP_V4_ROUTER_SOL      0x0a
+#define ICMP_V4_TIMEOUT         0x0b
+#define ICMP_V4_MALFORMED       0x0c
+
+struct icmp_v4 {
+    uint8_t type;
+    uint8_t code;
+    uint16_t csum;
+    union {
+        struct {
+            uint8_t id;
+            uint8_t seq;
+        } echo;
+    };
+    uint8_t data[];
+} __attribute__((packed));
+
 void icmpv4_incoming(struct netdev *netdev, struct eth_hdr *hdr);
+void icmpv4_reply(struct netdev *netdev, struct eth_hdr *hdr);
 
 #endif
--- src/icmpv4.c
+++ src/icmpv4.c
@@ -1,6 +1,29 @@
 #include "icmpv4.h"
+#include "ipv4.h"
 
 void icmpv4_incoming(struct netdev *netdev, struct eth_hdr *hdr) 
 {
-    printf("stub\n");
+    struct iphdr *iphdr = (struct iphdr *) hdr->payload;
+    struct icmp_v4 *icmp = (struct icmp_v4 *) iphdr->data;
+
+    //TODO: Check csum
+
+    switch (icmp->type) {
+        case ICMP_V4_ECHO:
+            icmpv4_reply(netdev, hdr);
+            break;
+        default:
+            perror("ICMPv4 did not match supported types");
+            return;
+    }
+}
+
+void icmpv4_reply(struct netdev *netdev, struct eth_hdr *hdr)
+{
+    struct iphdr *iphdr = (struct iphdr *) hdr->payload;
+    struct icmp_v4 *icmp = (struct icmp_v4 *) iphdr->data;
+
+    icmp->type = ICMP_V4_REPLY;
+
+    ipv4_outgoing(netdev, hdr);
 }
