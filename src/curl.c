66bc6279585280673e532a90ff0c592f3e1e8122
Sami Niiranen <saminiir@gmail.com>
2016-06-11 15:57:34 +0300

    Resolve hostname for curl
    Not sure if the strnlen and MAX_HOSTNAME is enough, from security
    point of view.

--- src/curl.c
+++ src/curl.c
@@ -1,17 +1,29 @@
 #include "syshead.h"
 
+#define MAX_HOSTNAME 50
+
 int curl_init()
 {
-	/* if (curl) printf("%hhu.%hhu.%hhu.%hhu\n", addr.sa_data[2], addr.sa_data[3], addr.sa_data[4], addr.sa_data[5]); */
+
 
     struct sockaddr addr;
             /* craft_curl_packet(&netdev, addr.sa_data[2], 4); */
 	return 0;
 }
 
-void curl(int argc, char **argvx)
+void curl(int argc, char **argv)
 {
-	printf("Test\n");
+	if (argc != 3 || strnlen(argv[2], MAX_HOSTNAME) == MAX_HOSTNAME) {
+		print_error("Curl called but HOST not given or invalid\n");
+		exit(1);
+	}
 
-	return NULL;
+	struct sockaddr addr;
+
+	if (get_address(argv[2], &addr) != 0) {
+		print_error("Curl could not resolve hostname\n");
+		exit(1);
+	}
+	
+	printf("%hhu.%hhu.%hhu.%hhu\n", addr.sa_data[2], addr.sa_data[3], addr.sa_data[4], addr.sa_data[5]);
 }
