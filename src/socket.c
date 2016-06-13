59393d846953e9f5178017f30e2e71583471e1c5
Sami Niiranen <saminiir@gmail.com>
2016-06-13 20:09:28 +0300

    Add error checking for tcp sock alloc

--- src/socket.c
+++ src/socket.c
@@ -1,9 +1,16 @@
 #include "syshead.h"
+#include "utils.h"
 #include "tcp_socket.h"
 
 int socket(int domain, int type, int protocol)
 {
-    struct tcp_socket *sock = alloc_tcp_socket();
+    struct tcp_socket *sock;
+
+    if ((sock = alloc_tcp_socket()) == NULL) {
+        print_error("Could not alloc socket\n");
+        exit(1);
+    }
+    
     printf("domain %x\n", domain);
     printf("type %x\n", type);
     printf("protocol %x\n", protocol);
