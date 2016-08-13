07c1807ee2791d481761cc1234cea5c2d0473d23
Sami Niiranen <saminiir@gmail.com>
2016-08-13 14:19:27 +0300

    Introduce ipv4 address to int parse routine

--- include/utils.h
+++ include/utils.h
@@ -9,5 +9,6 @@ void print_error(char *str, ...);
 uint32_t sum_every_16bits(void *addr, int count);
 uint16_t checksum(void *addr, int count, int start_sum);
 int get_address(char *host, struct sockaddr *addr);
+uint32_t parse_ipv4_string(char *addr);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -98,3 +98,9 @@ int get_address(char *host, struct sockaddr *addr)
     
     return 1;
 }
+
+uint32_t parse_ipv4_string(char* addr) {
+    uint8_t addr_bytes[4];
+    sscanf(addr, "%hhu.%hhu.%hhu.%hhu", &addr_bytes[3], &addr_bytes[2], &addr_bytes[1], &addr_bytes[0]);
+    return addr_bytes[0] | addr_bytes[1] << 8 | addr_bytes[2] << 16 | addr_bytes[3] << 24;
+}
