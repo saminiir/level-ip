a182264119ab3e5b32eb8735cb41982f914a6425
Sami Niiranen <saminiir@gmail.com>
2016-11-06 21:42:15 +0200

    Add minimal doubly circular linked list implementation
    This is taken pretty much straight from Git's source code.

---
+++ include/list.h
@@ -0,0 +1,61 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+struct list_head {
+    struct list_head *next;
+    struct list_head *prev;
+};
+
+#define LIST_HEAD(name) \
+    struct list_head name = { &(name), &(name) }
+
+static inline void list_init(struct list_head *head)
+{
+    head->prev = head->next = head;
+}
+
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+    head->next->prev = new;
+    new->next = head->next;
+    new->prev = head;
+    head->next = new;
+}
+
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+    head->prev->next = new;
+    new->prev = head->prev;
+    new->next = head;
+    head->prev = new;
+}
+
+static inline void list_del(struct list_head *elem)
+{
+    struct list_head *prev = elem->prev;
+    struct list_head *next = elem->next;
+
+    prev->next = next;
+    next->prev = prev;
+}
+
+#define list_entry(ptr, type, member) \
+    ((type *) ((char *) (ptr) - offsetof(type, member)))
+
+#define list_first_entry(ptr, type, member) \
+    list_entry((ptr)->next, type, member)
+
+#define list_for_each(pos, head) \
+    for (pos = (head)->next; pos != (head); pos = pos->next)
+
+#define list_for_each_safe(pos, p, head)    \
+    for (pos = (head)->next, p = pos->next; \
+         pos != (head);                     \
+         pos = p, p = pos->next)
+
+static inline int list_empty(struct list_head *head)
+{
+    return head->next == head;
+}
+
+#endif
