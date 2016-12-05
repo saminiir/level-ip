81f1b6e353f075183b91b48b5004b597313449fb
Sami Niiranen <saminiir@gmail.com>
2016-12-05 12:44:45 +0200

    Introduce route entry struct

--- include/route.h
+++ include/route.h
@@ -2,6 +2,17 @@
 #define _ROUTE_H
 
 #include "dst.h"
+#include "list.h"
+
+struct rtentry {
+    struct list_head list;
+    uint32_t dst;
+    uint32_t gateway;
+    uint32_t netmask;
+    uint8_t flags;
+    uint32_t metric;
+    struct netdev *dev;
+};
 
 struct rtable {
     struct dst_entry dst;
