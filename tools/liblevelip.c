8727cef1fc3bc8a3800d4f7e96bcc06f4f021666
Sami Niiranen <saminiir@gmail.com>
2016-12-10 14:55:24 +0200

    Hijack libc_start_main
    This is defined in Linux Standard Base:
    
    http://refspecs.linuxbase.org/LSB_3.1.1/LSB-Core-generic/LSB-Core-generic/baselib---libc-start-main-.html

--- tools/liblevelip.c
+++ tools/liblevelip.c
@@ -3,19 +3,29 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
+static int (*__start_main)(int (*main) (int, char * *, char * *), int argc, \
+                           char * * ubp_av, void (*init) (void), void (*fini) (void), \
+                           void (*rtld_fini) (void), void (* stack_end));
+
 static int (*_connect)(int sockfd, const struct sockaddr *addr, socklen_t addrlen) = NULL;
 static int (*_socket)(int domain, int type, int protocol) = NULL;
 
 int socket(int domain, int type, int protocol)
 {
-    _socket = dlsym(RTLD_NEXT, "socket");
-
     return _socket(domain, type, protocol);
 }
 
 int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
-    _connect = dlsym(RTLD_NEXT, "connect");
-
     return _connect(sockfd, addr, addrlen);
+}
+
+int __libc_start_main(int (*main) (int, char * *, char * *), int argc, char * * ubp_av, void (*init) (void), void (*fini) (void), void (*rtld_fini) (void), void (* stack_end))
+{
+    __start_main = dlsym(RTLD_NEXT, "__libc_start_main");
+
+    _connect = dlsym(RTLD_NEXT, "connect");
+    _socket = dlsym(RTLD_NEXT, "socket");
+ 
+    return __start_main(main, argc, ubp_av, init, fini, rtld_fini, stack_end);
 }
