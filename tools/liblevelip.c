253e5c7c1aacd6ff451bd6e571f62aa8aa5e5e7a
Sami Niiranen <saminiir@gmail.com>
2016-12-10 14:53:12 +0200

    Overwrite socket libc function call

--- tools/liblevelip.c
+++ tools/liblevelip.c
@@ -4,6 +4,14 @@
 #include <dlfcn.h>
 
 static int (*_connect)(int sockfd, const struct sockaddr *addr, socklen_t addrlen) = NULL;
+static int (*_socket)(int domain, int type, int protocol) = NULL;
+
+int socket(int domain, int type, int protocol)
+{
+    _socket = dlsym(RTLD_NEXT, "socket");
+
+    return _socket(domain, type, protocol);
+}
 
 int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
