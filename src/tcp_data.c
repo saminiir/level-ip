26855ca4c4bf124b31d4457ab9817d372990a2fd
Sami Niiranen <saminiir@gmail.com>
2017-04-16 12:07:34 +0300

    Make delack packet length threshold more aggressive
    This is temporary until we implement parsing of peer's MSS.
    Then we know the actual MSS threshold.

--- src/tcp_data.c
+++ src/tcp_data.c
@@ -118,7 +118,7 @@ int tcp_data_queue(struct tcp_sock *tsk, struct tcphdr *th, struct sk_buff *skb)
          * be excessively delayed; in particular, the delay MUST be less than
          * 0.5 seconds, and in a stream of full-sized segments there SHOULD 
          * be an ACK for at least every second segment. */
-        if (th->psh || (skb->dlen == tsk->rmss && ++tsk->delacks > 1)) {
+        if (th->psh || (skb->dlen > 1000 && ++tsk->delacks > 1)) {
             tsk->delacks = 0;
             tcp_send_ack(sk);
         } else {
